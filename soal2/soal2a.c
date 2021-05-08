e4fb6b8e64f93be6e8b96fa14fcdf7453209cde8
rickys20 <73892020+rickys20@users.noreply.github.com>
2021-05-08 10:41:50 +0700

    Update soal2a.c

--- soal2/soal2a.c
+++ soal2/soal2a.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
  
 int main()
 {
-  int m, n, p, q, c, d, k, sum = 0;
-  int first[10][10], second[10][10], multiply[10][10];
+    key_t key = 1234;
+  // int *value;
+    int (*value)[6];
+    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+    value = shmat(shmid, NULL, 0);
 
-  // matriks pertama 4*3 c(baris) d(kolom)
-  printf("Masukan data Matrix 2:\n");
-  for (  c = 0 ; c < 4 ; c++ )
-  {
-    for ( d = 0 ; d < 3 ; d++ )
-    {
-      scanf("%d", &first[c][d]);
-    }
-  }
-  printf("Masukan data Matrix 2:\n");
-  for ( c = 0 ; c < 3 ; c++ )
-  {
-    for ( d = 0 ; d < 6 ; d++ )
-    {
-      scanf("%d", &second[c][d]);
-    }
-  }
+    int c, d, k, sum = 0;
+  //  int first[10][10], second[10][10], 
+    int multiply[10][10];
+
+    int first[4][3] = {{1, 2, 1},
+                    {2, 1, 3},
+                    {2, 1, 1},
+                    {1, 1, 1}};
+    int second[3][6] = {{1, 2, 2, 1, 1, 1},
+                      {2, 2, 2, 2, 1},
+                      {1, 1, 1, 2, 1}};
+
+  // // matriks pertama 4*3 c(baris) d(kolom)
+  // printf("Masukan data Matrix:\n");
+  // for (  c = 0 ; c < 4 ; c++ )
+  // {
+  //   for ( d = 0 ; d < 3 ; d++ )
+  //   {
+  //     scanf("%d", &first[c][d]);
+  //   }
+  // }
+  // printf("Masukan data Matrix 2:\n");
+  // for ( c = 0 ; c < 3 ; c++ )
+  // {
+  //   for ( d = 0 ; d < 6 ; d++ )
+  //   {
+  //     scanf("%d", &second[c][d]);
+  //   }
+  // }
   //hasil
   for ( c = 0 ; c < 4 ; c++ )
   {
@@ -42,22 +60,13 @@ int main()
   {
     for ( d = 0 ; d < 6 ; d++ )
     {
+      value[c][d] = multiply[c][d];
       printf("%d\t", multiply[c][d]);
     }
     printf("\n");
   }
- 
-  return 0;
+    shmdt(value);
+    // shmctl(shmid, IPC_RMID, NULL);
+
+	return 0;
 }
-
-
-// cek
-//   1 2 3 
-//   1 2 3 
-//   2 2 2 
-//   2 1 2
-
-// 1 2 3 4 1 2
-// 1 2 3 1 3 3
-// 1 2 2 2 3 3
-
