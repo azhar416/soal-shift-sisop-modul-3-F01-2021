8f0af5caf07e6b81b2449b7a2d14943fa580bc9c
inezamanda <inez.amanda16@gmail.com>
2021-05-07 23:46:45 -0700

    command -f done

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -39,7 +39,8 @@ char * get_filename_ext(char *filename) {
     char * extname = (char*)malloc(sizeof(char)* (PATH_MAX + PATH_MAX));
     memset(extname,0,sizeof(char)* (PATH_MAX + PATH_MAX));
     char *dotted = strrchr(filename, '/');
-    if(dotted[0] == "."){
+    //memmove(&dotted[0],&dotted[1],strlen(dotted) - 0);
+    if(dotted[0] == '.'){
         strcpy(extname,"Hidden");
         return extname;
     }
@@ -62,31 +63,64 @@ void* checkFolderAndCopy(void* args){
     strcat(pathname,"/");
     strcat(pathname,extensionName);
     mkdir(pathname,0777);
-    // int val = mkdir(pathname,0777);
-    // if(val == EEXIST){
-    //     printf("Directory made!\n");
-    // }
     pthread_mutex_lock(&bufferlock);
     strcat(pathname,"/");
     char buffer[PATH_MAX + PATH_MAX];
     memset(buffer,0,sizeof(buffer));
     strcpy(buffer,pathname);
     strcat(buffer,basename(filenow->filename));
-    // printf("moving %s to %s\n",filenow->filename,buffer);
+    printf("moving %s to %s\n",filenow->filename,buffer);
     rename(filenow->filename,buffer);
     pthread_mutex_unlock(&bufferlock);
     // remove(filenow->filename);
 }
 
+int ix;
+
+void listFilesRecursive(char *base, pthread_t *thread) {
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(base);
+
+    if (!dir)
+        return;
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        {
+            // Construct new path from our base path
+            strcpy(path, base);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+            file_t * filenow = (file_t*)malloc(sizeof(file_t));
+            if (dp->d_type == DT_REG)
+            {
+                strcpy(filenow->filename,path);
+                strcpy(filenow->curDir,"/sisop/soal-shift-sisop-modul-3-F01-2021/soal3");
+                if (strcmp(path,"/sisop/soal-shift-sisop-modul-3-F01-2021/soal3/soal3.c") != 0)
+                {
+                    pthread_create(&thread[ix],NULL,checkFolderAndCopy,(void*)filenow);
+                    sleep(1);
+                    ix++;
+                }
+            }
+            else if (dp->d_name[0] != '.')
+                listFilesRecursive(path, thread);
+        }
+    }
+
+    closedir(dir);
+}
 
 int main(int argc, char * argv[]){
     char curDir[PATH_MAX + PATH_MAX];
     getcwd(curDir,sizeof(curDir));
     pthread_mutex_init(&bufferlock,NULL);
-
+    pthread_t copy_thread[1000];
     // return 0;
     if(strcmp(argv[1],"-f")==0){
-        pthread_t copy_thread[argc];
+        //pthread_t copy_thread[argc];
         for(int i=2;i<argc;i++){
             file_t * filenow = (file_t*)malloc(sizeof(file_t));
             filenow->curDir = curDir;
@@ -94,68 +128,23 @@ int main(int argc, char * argv[]){
             memset(copy,0,sizeof(char)*strlen(argv[i]));
             strcpy(copy,argv[i]);
             filenow->filename = copy;
-            // printf("%s\n",get_filename_ext(filenow->filename));
 
             int iret = pthread_create(&copy_thread[i],NULL,checkFolderAndCopy,(void*)filenow);
+            // if(is_regular_file(argv[i])){
             if(iret){
-                // perror("thread1");
+                perror("thread1");
+                //iret;
                 printf("File %d: Sad, gagal :(\n", i-1);
                 exit(EXIT_FAILURE);
             }
             else {
                 printf("File %d: Berhasil Dikategorikan\n", i - 1);
                 sleep(1);
-                ++i;
+                ix++;
             }
         }
         for(int i=2;i<argc;i++){
             pthread_join(copy_thread[i],NULL);
-        }
-    }else if(strcmp(argv[1],"*")==0){
-        int count=0;
-        DIR *d;
-        struct dirent *dir;
-        d = opendir(".");
-        if (d){
-            char buffer[PATH_MAX + PATH_MAX];
-            memset(buffer,0,sizeof(buffer));
-            while ((dir = readdir(d)) != NULL){
-                if(is_regular_file(dir->d_name)){
-                    count++;
-                }
-            }
-            closedir(d);
-        }
-
-        pthread_t copy_thread[count];
-        int i=0;
-        d = opendir(".");
-        if (d){
-            char buffer[PATH_MAX + PATH_MAX];
-            memset(buffer,0,sizeof(buffer));
-            while ((dir = readdir(d)) != NULL){
-                if(is_regular_file(dir->d_name)){
-                    realpath(dir->d_name,buffer);
-
-                    file_t * filenow = (file_t*)malloc(sizeof(file_t));
-                    filenow->curDir = curDir;
-                    char * copy = (char*)malloc(sizeof(char)* (PATH_MAX + PATH_MAX));
-                    memset(copy,0,sizeof(char)* (PATH_MAX + PATH_MAX));
-                    strcpy(copy,buffer);
-                    filenow->filename = copy;
-
-                    int iret = pthread_create(&copy_thread[i],NULL,checkFolderAndCopy,(void*)filenow);
-                    if(iret){
-                        perror("thread1");
-                        exit(EXIT_FAILURE);
-                    }
-                    i++;
-                }
-            }
-            closedir(d);
-        }
-        for(int j=0;j<count;j++){
-            pthread_join(copy_thread[j],NULL);
         }
     }else if(strcmp(argv[1],"-d")==0){
         chdir(argv[2]);
@@ -172,34 +161,22 @@ int main(int argc, char * argv[]){
             closedir(d);
         }
 
-        pthread_t copy_thread[count];
         int i=0;
         d = opendir(".");
         if (d){
-            char buffer[PATH_MAX + PATH_MAX];
-            memset(buffer,0,sizeof(buffer));
-            while ((dir = readdir(d)) != NULL){
-                if(is_regular_file(dir->d_name)){
-                    realpath(dir->d_name,buffer);
-
-
-                    file_t * filenow = (file_t*)malloc(sizeof(file_t));
-                    filenow->curDir = curDir;
-                    char * copy = (char*)malloc(sizeof(char)* (PATH_MAX + PATH_MAX));
-                    memset(copy,0,sizeof(char)* (PATH_MAX + PATH_MAX));
-                    strcpy(copy,buffer);
-                    filenow->filename = copy;
-                    int iret = pthread_create(&copy_thread[i],NULL,checkFolderAndCopy,(void*)filenow);
-                    if(iret){
-                        perror("thread1");
-                        exit(EXIT_FAILURE);
-                    }
-                }
-            }
-            closedir(d);
+            listFilesRecursive(argv[2], copy_thread);
+            if (count == 0)
+                printf("Yah, gagal disimpan :(\n");
+            
+            else printf("Direktori Sukses Disimpan!\n");
         }
-        for(int j=0;j<count;j++){
-            pthread_join(copy_thread[j],NULL);
+    }else if(strcmp(argv[1],"*")==0){
+        int count=0;
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(curDir);
+        if (d){
+            listFilesRecursive(curDir, copy_thread);
         }
     }else{
         return 0;
